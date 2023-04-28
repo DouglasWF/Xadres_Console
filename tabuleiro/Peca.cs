cfe3eb0036ee007c148e77453b2dd7e7d7214e24
Douglas Freitas <douglas_freitas@outlook.com>
2023-04-28 15:25:21 -0300

    Fazendo os primeiros teste de movimentacao

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     internal class Peca
     {
@@ -13,18 +7,18 @@ namespace tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; set; }
 
-        public Peca(Cor cor, Tabuleiro tab)
-        {
-            this.posicao = null;
-            this.tab = tab;
-            this.cor = cor;
-            this.qteMovimentos = 0;
-        }
-
+        
         public Peca(Tabuleiro tab, Cor cor)
         {
             this.tab = tab;
             this.cor = cor;
+            this.qteMovimentos = 0;
+            this.posicao = null;
+        }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
         }
     }
 }
